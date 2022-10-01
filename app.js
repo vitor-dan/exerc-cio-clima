e8ca18740107b4151c303ce588b017687a87c61c
Vitor Daniel Benedito Santo <vitor_daniel85@outlook.com.br>
2022-10-01 17:40:02 -0300

    Removido console.log

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ button.addEventListener('click', getWeather)
 function getWeather() {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherToken}&lang=pt_br`)
     .then(function (response) {
-        console.log(response)
         addLine(response.data.name)
         addLine(response.data.weather[0].description)
     }).catch(function (error) {
